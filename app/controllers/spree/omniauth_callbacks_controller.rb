23b06737b4a4e5d4b6ba918d0b93c175e5f02cb8
Rahul Ranchal <rahul.ranchal@skyach.com>
2020-05-07 15:39:23 +0530

    Update omniauth_callbacks_controller.rb

--- app/controllers/spree/omniauth_callbacks_controller.rb
+++ app/controllers/spree/omniauth_callbacks_controller.rb
@@ -3,6 +3,7 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   include Spree::Core::ControllerHelpers::Order
   include Spree::Core::ControllerHelpers::Auth
   include Spree::Core::ControllerHelpers::Store
+  skip_before_action :verify_authenticity_token
 
   def self.provides_callback_for(*providers)
     providers.each do |provider|
